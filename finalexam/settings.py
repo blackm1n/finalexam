6551b1aeb347137c04fb3ae700f79b78a68e9b0c
Black <modnyak0001@mail.ru>
2024-03-12 16:45:56 +1000

    Attempt 3

--- finalexam/settings.py
+++ finalexam/settings.py
@@ -32,7 +32,7 @@ CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'blackm4n.pythonanywhere.com',
+    'blackm5n.pythonanywhere.com',
 ]
 
 
@@ -86,10 +86,10 @@ WSGI_APPLICATION = 'finalexam.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blackm4n$default',
-        'USER': 'blackm4n',
+        'NAME': 'blackm5n$default',
+        'USER': 'blackm5n',
         'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': 'blackm4n.mysql.pythonanywhere-services.com',
+        'HOST': 'blackm5n.mysql.pythonanywhere-services.com',
         'OPTIONS': {
             'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
